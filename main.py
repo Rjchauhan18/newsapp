ada336d0acc4675c665f26f6e84eb208b9de0800
Rjchauhan18 <rjchauhan5000000@gmail.com>
2023-09-14 21:27:50 +0530

    done

--- main.py
+++ main.py
@@ -80,7 +80,7 @@ def News(symbol):
 
     #news section 
     try:
-        NEWS = get_Data.news
+        NEWS = get_Data.news()
         st.write(NEWS)
         st.header(f"News of {select_company} :")
         for i in range(len(NEWS)):
